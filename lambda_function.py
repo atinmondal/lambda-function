75b1837974920879f805e1571d6e243add2c21f7
Atin Mondal <atinmondal@Atins-MacBook-Air.local>
2025-11-14 21:49:48 +0530

    update

--- lambda_function.py
+++ lambda_function.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello from GitHub->AWS Lambda!- new changes",
+            "message": "Hello from GitHub->AWS Lambda integration! Atin Mondal.",
             "received_event": event,
             "invoked_at": datetime.datetime.utcnow().isoformat() + "Z"
         })
