81a1bd80a2356d0527b3be53914d6d3018af5f76
Atin Mondal <atinmondal@Atins-MacBook-Air.local>
2025-11-14 21:42:51 +0530

    update

--- lambda_function.py
+++ lambda_function.py
@@ -10,7 +10,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello from GitHub->AWS Lambda!",
+            "message": "Hello from GitHub->AWS Lambda!- new changes",
             "received_event": event,
             "invoked_at": datetime.datetime.utcnow().isoformat() + "Z"
         })
